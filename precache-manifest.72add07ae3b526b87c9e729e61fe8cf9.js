9a70f7a6410ba04c2a18d5c863e1172a8d25cfc2
Azer N. Karimzada <KarimzadaAN@kapitalho.az>
2020-05-07 15:09:11 +0400

    Updates

---
+++ precache-manifest.72add07ae3b526b87c9e729e61fe8cf9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26fe353bfefea6afa0f4c5ac4053cb2",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "43c291831e446b3c1b21",
+    "url": "/React/static/css/main.c7983cc9.chunk.css"
+  },
+  {
+    "revision": "3488c401d4e9649c5510",
+    "url": "/React/static/js/2.98c2b596.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React/static/js/2.98c2b596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c291831e446b3c1b21",
+    "url": "/React/static/js/main.08df2f48.chunk.js"
+  },
+  {
+    "revision": "c913e1898f6d7d34d8dd",
+    "url": "/React/static/js/runtime-main.ef03d3a7.js"
+  },
+  {
+    "revision": "0932466454064396020bd50b8dedb76d",
+    "url": "/React/static/media/dog.09324664.jpg"
+  },
+  {
+    "revision": "87b8671c2d08dc83554806539022bde7",
+    "url": "/React/static/media/free-gif-preloaders-psds-09.87b8671c.gif"
+  },
+  {
+    "revision": "f8a15e74d9b85e04019c7efd5ae76dff",
+    "url": "/React/static/media/user.f8a15e74.png"
+  }
+]);
